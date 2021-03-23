69464b52fddd296a5af449cba4b1f8cebc317d5a
Emily Borgers <e.borgers2@kennisnet.nl>
2021-03-23 15:22:35 +0100

    CATWEB-36: Removed restraints of maximum records and start record when using catalogus strategy and cleaned up tests.

--- tests/EdurepSearchTest.php
+++ tests/EdurepSearchTest.php
@@ -2,7 +2,6 @@
 
 namespace Tests\Kennisnet\Edurep;
 
-
 use Kennisnet\Edurep\DefaultSearchConfig;
 use Kennisnet\Edurep\EdurepSearch;
 use Kennisnet\Edurep\SearchClient;
@@ -13,30 +12,87 @@ use PHPUnit\Framework\TestCase;
 class EdurepSearchTest extends TestCase
 {
 
-    public function testInvalidMax(): void
+    public function test_exception_thrown_on_setting_invalid_maximum_records_value(): void
     {
+        $strategy = new EdurepStrategyType();
+        $config   = new DefaultSearchConfig($strategy, "http://wszoeken.edurep.kennisnet.nl:8000/");
+        $edurep   = new EdurepSearch($config);
+
         $this->expectExceptionMessage('The value for maximumRecords should be between 0 and 100.');
 
+        $edurep->setMaximumRecords(-1);
+    }
+
+    public function test_exception_thrown_on_missing_query_when_getting_request_url(): void
+    {
         $strategy = new EdurepStrategyType();
         $config   = new DefaultSearchConfig($strategy, "http://wszoeken.edurep.kennisnet.nl:8000/");
         $edurep   = new EdurepSearch($config);
 
-        $edurep->setMaximumRecords(-1);
-        $edurep->getRequestUrl();
-    }
-
-    public function testNoQuery(): void
-    {
         $this->expectExceptionMessage('Missing query');
 
+        $edurep->getRequestUrl();
+    }
+
+    public function test_catalogus_can_set_infinitely_high_start_record(): void
+    {
+        $strategy = new CatalogusStrategyType();
+        $config = new DefaultSearchConfig($strategy, "https://staging.catalogusservice.edurep.nl/");
+        $edurep = new EdurepSearch($config);
+
+        $edurep->setStartRecord(3000);
+        $edurep->setQuery("mbo");
+
+        $this->assertEquals(
+            'https://staging.catalogusservice.edurep.nl/sru?operation=searchRetrieve&version=1.2&recordPacking=xml&query=mbo&maximumRecords=100&startRecord=3000',
+            $edurep->getRequestUrl(),
+            "Test catalogus URL with startRecord 3000"
+        );
+
+        $this->assertEquals(3000, $edurep->getParameters()['startRecord'], "Test catalogus can use unrestrained start record");
+    }
+
+    public function test_catalogus_can_set_infinitely_high_maximum_records(): void
+    {
+        $strategy = new CatalogusStrategyType();
+        $config   = new DefaultSearchConfig($strategy, "https://staging.catalogusservice.edurep.nl/");
+        $edurep   = new EdurepSearch($config);
+
+        $edurep->setMaximumRecords(1000);
+        $edurep->setQuery("mbo");
+
+        $this->assertEquals(
+            'https://staging.catalogusservice.edurep.nl/sru?operation=searchRetrieve&version=1.2&recordPacking=xml&query=mbo&maximumRecords=1000',
+            $edurep->getRequestUrl(),
+            "Test catalogus URL with maximumRecords 1000"
+        );
+
+        $this->assertEquals(1000, $edurep->getParameters()['maximumRecords'], "Test catalogus can use unrestrained maxiumum records");
+    }
+
+    public function test_edurep_cant_use_unrestrained_maximum_records(): void
+    {
         $strategy = new EdurepStrategyType();
         $config   = new DefaultSearchConfig($strategy, "http://wszoeken.edurep.kennisnet.nl:8000/");
         $edurep   = new EdurepSearch($config);
 
-        $edurep->getRequestUrl();
+        $this->expectExceptionMessage('The value for maximumRecords should be between 0 and 100.');
+
+        $edurep->setMaximumRecords(1000);
     }
 
-    public function testEdurepQuery(): void
+    public function test_edurep_cant_use_unrestrained_start_record(): void
+    {
+        $strategy = new EdurepStrategyType();
+        $config   = new DefaultSearchConfig($strategy, "http://wszoeken.edurep.kennisnet.nl:8000/");
+        $edurep   = new EdurepSearch($config);
+
+        $this->expectExceptionMessage('The value for startRecords should be between 1 and 1000.');
+
+        $edurep->setStartRecord(3000);
+    }
+
+    public function test_edurep_query(): void
     {
         $strategy = new EdurepStrategyType();
         $config   = new DefaultSearchConfig($strategy, "http://wszoeken.edurep.kennisnet.nl:8000/");
@@ -76,7 +132,7 @@ class EdurepSearchTest extends TestCase
         );
     }
 
-    public function testCatalogQuery(): void
+    public function test_catalogus_query(): void
     {
         $strategy = new CatalogusStrategyType();
         $config   = new DefaultSearchConfig($strategy, "https://staging.catalogusservice.edurep.nl/");
@@ -101,7 +157,7 @@ class EdurepSearchTest extends TestCase
         );
     }
 
-    public function testDefaultSearch()
+    public function test_default_search()
     {
         $strategy     = new EdurepStrategyType();
         $config       = new DefaultSearchConfig($strategy, "http://wszoeken.edurep.kennisnet.nl:8000/");
